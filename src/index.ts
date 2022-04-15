b8b9215586073719e91d6a466acbf34696ffb889
Alessandro Solbiati <lessandro@fb.com>
2022-04-15 16:13:20 +0100

    [expressjs] boilerplate

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import {GGraph} from './ggraph';
+
+const container = document.getElementById("sigma-container") as HTMLElement;
+
+const graph = new GGraph();
+
+graph.addNode("John", { x: 0, y: 10, size: 5, label: "John", color: "blue" });
+graph.addNode("Mary", { x: 10, y: 0, size: 3, label: "Mary", color: "red" });
+
+graph.addEdge('John', 'Mary');
+
+console.log(graph.order); //nodes
+console.log(graph.size); //edges
+
+graph.forEachNode(node => {
+	console.log(node);
+})
