080defeb24c2ed9d439e45f8df25efa473dbb541
Alessandro Solbiati <lessandro@fb.com>
2022-04-15 18:00:28 +0100

    [build] cleaned

--- build/index.js
+++
@@ -1,5 +0,0 @@
-import { createGraph } from './graph';
-import Sigma from "sigma";
-var container = document.getElementById("sigma-container");
-var graph = createGraph();
-var renderer = new Sigma(graph, container);
